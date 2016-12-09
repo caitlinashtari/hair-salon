4fa63bb383b93151decaf9ab672e39dbe50e96d3
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-09 09:14:51 -0800

    add save and == methods to client class with passing specs.

--- hair_salon/lib/client.rb
+++ hair_salon/lib/client.rb
@@ -5,7 +5,7 @@ class Client
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
     @appointment_time = attributes.fetch(:appointment_time)
-    @stylist_id = attributes.fetch(:stylist_id)
+    @stylist_id = attributes.fetch(:stylist_id).to_i
   end
 
   define_singleton_method(:all) do
@@ -15,10 +15,19 @@ class Client
       name = client.fetch("name")
       id = client.fetch("id").to_i
       appointment_time = client.fetch("appointment_time")
-      stylist_id = client.fetch("stylist_id")
+      stylist_id = client.fetch("stylist_id").to_i
       clients.push(Client.new({:name => name, :id => id, :appointment_time => appointment_time, :stylist_id => stylist_id}))
     end
     clients
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (name, appointment_time, stylist_id) VALUES ('#{@name}', '#{@appointment_time}', #{@stylist_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_client|
+    self.name().==(another_client.name()).&(self.id().==(another_client.id())).&(self.stylist_id().==(another_client.stylist_id()))
   end
 
 end
--- hair_salon/spec/client_spec.rb
+++ hair_salon/spec/client_spec.rb
@@ -3,17 +3,33 @@ require('spec_helper')
 describe(Client) do
   describe('#initialize') do
     it('will instantiate a new client with name and id attributes') do
-      client = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => nil})
+      client = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => 1})
       expect(client.name).to(eq("Blam"))
       expect(client.id).to(eq(nil))
       expect(client.appointment_time).to(eq("2016-12-12 00:12:00"))
-      expect(client.stylist_id).to(eq(nil))
+      expect(client.stylist_id).to(eq(1))
     end
   end
 
   describe('.all') do
     it('starts off with no clients') do
       expect(Client.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('lets you save the client to the database') do
+      client = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => 1})
+      client.save
+      expect(Client.all).to(eq([client]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same name, ID, and stylist ID") do
+      client1 = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => 1})
+      client2 = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => 1})
+      expect(client1).to(eq(client2))
     end
   end
 
