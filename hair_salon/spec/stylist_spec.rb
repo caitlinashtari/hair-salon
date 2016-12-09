ef1de6596a3f790c9e0194ab8593ce7b146e702e
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-09 08:14:51 -0800

    add delete method to stylist class with passing spec.

--- hair_salon/spec/stylist_spec.rb
+++ hair_salon/spec/stylist_spec.rb
@@ -40,4 +40,15 @@ describe(Stylist) do
     end
   end
 
+  describe("#delete") do
+    it("lets you delete a stylist from the database") do
+      stylist = Stylist.new({:name => "Samantha", :id => nil})
+      stylist.save()
+      stylist2 = Stylist.new({:name => "Bob", :id => nil})
+      stylist2.save()
+      stylist.delete()
+      expect(Stylist.all()).to(eq([stylist2]))
+    end
+  end
+
 end
