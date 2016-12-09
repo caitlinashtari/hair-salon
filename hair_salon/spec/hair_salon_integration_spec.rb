c89525d40e368fa458dbfaf7a3e74e66e44db022
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-09 11:30:59 -0800

    add passing integration spec for viewing a stylists clients.

--- hair_salon/spec/hair_salon_integration_spec.rb
+++ hair_salon/spec/hair_salon_integration_spec.rb
@@ -12,14 +12,14 @@ describe('adding a new stylist', {:type => :feature}) do
   end
 end
 
-# describe('seeing details for a single stylist', {:type => :feature}) do
-#   it('allows a user to click on a stylist to view their details') do
-#     stylist = Stylist.new({:name => "Sam", :id => nil})
-#     stylist.save
-#     client = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => 1})
-#     client.save
-#     visit('/')
-#     click_link(stylist.name)
-#     expect(page).to have_content(stylist.name)
-#   end
-# end
+describe('seeing details for a single stylist', {:type => :feature}) do
+  it('allows a user to click on a stylist to view their details') do
+    stylist = Stylist.new({:name => "Sam", :id => nil})
+    stylist.save
+    client = Client.new({:name => "Blam", :id => nil, :appointment_time => "2016-12-12 00:12:00", :stylist_id => stylist.id})
+    client.save
+    visit('/')
+    click_link(stylist.name)
+    expect(page).to have_content(stylist.name)
+  end
+end
